94eaa2aaab6ea3a4f210ced4a6b91e20e4559529
James Rynn Berry <jb5260@nyu.edu>
2021-05-31 16:06:10 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const cards = [];
+
+function writeCards(array){
+  for(let i=0; i<array.length; i++){
+  
+    cards.message = `Thank you, ${array[i]}, for the wonderful surprise gift!`
+    cards.push(`Thank you, ${array[i]}, for the wonderful surprise gift!`)
+  }
+  return cards;
+}
+writeCards(names, "eventName")
+
+function countDown(init = 10){
+  while (init >= 0) {
+    console.log(init--)
+  }
+}
